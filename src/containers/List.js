834a77430f97e7dcde846a5529ff0cac913a26ba
ctola91 <ctolapacheco@gmail.com>
2021-04-24 15:28:20 -0400

    add movies list

---
+++ src/containers/List.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card/Card";
+
+const List = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const movies = await fetch("../../assets/data.json");
+      const moviesJSON = await movies.json();
+
+      if (moviesJSON) {
+        setData(moviesJSON);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <div>Loading</div>
+  ) : (
+    <div className="row">
+      {data.map((movie) => (
+        <div className="col-sm-2">
+        <Card key={movie.id} movie={movie} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
